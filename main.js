23f430774fc719999b408af5ae26a557508b1865
im-ivory <78600276+im-ivory@users.noreply.github.com>
2021-07-28 04:21:20 +0900

    second commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const mobileNav = document.getElementById("mobileNav");
+
+function openNav() {
+    mobileNav.style.height = "100%";
+  }
+  
+  function closeNav() {
+    mobileNav.style.height = "0%";
+  }
