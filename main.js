118a5e0464b03bea3d5157262bbeecc8270a220f
im-ivory <78600276+im-ivory@users.noreply.github.com>
2021-07-28 20:57:21 +0900

    Third Commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const mobileNav = document.getElementById("mobileNav");
+const pcNav = document.querySelector(".pc-nav");
+const dropDown = document.querySelector(".drop-down");
 
 function openNav() {
     mobileNav.style.height = "100%";
@@ -7,3 +9,34 @@ function openNav() {
   function closeNav() {
     mobileNav.style.height = "0%";
   }
+
+  function showDropDown(){
+    dropDown.style.height = "10vw";
+  }
+  function closeDropDown(){
+    dropDown.style.height = "0%";
+  }
+
+dropDown.addEventListener("mouseleave", closeDropDown);
+ pcNav.addEventListener("mouseenter", showDropDown);
+
+const imgWrapper = document.querySelector(".img-wrapper");
+const img = Array.from(imgWrapper.getElementsByTagName("img"));
+
+
+    function changeOpacity(){
+      for (i=0; i<img.length; i++){
+        let imgRect = img[i].getBoundingClientRect();
+
+         if(imgRect.x > 362){
+            img[i].style.opacity =1;
+          }else if(imgRect.x < 300){
+            img[i].style.opacity =0.5;
+           } else if(imgRect.x < 23){
+              img[i].style.opacity =0.2;
+          }
+      }
+    }
+
+
+  imgWrapper.addEventListener("scroll", changeOpacity);
